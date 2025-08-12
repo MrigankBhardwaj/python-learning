b2df49705bdbd930bf89dd4e4aebb347c376d7f7
MrigankKumar <bhardwajmrigank13@gmail.com>
2025-08-12 22:55:14 +0530

    Second day

---
+++ third.py
@@ -0,0 +1,6 @@
+# In the third day I have learned about how you run the pyhton code in the cmd(command prompt)
+# First you have to open your CMd in any operating system you are .. Make sure Python is installed in your system
+# For windows
+# click windows button and search "cmd"
+# Then open CMD and type "python" and click enter
+# Then it will open your pyhon editor and you can write python code in it.
