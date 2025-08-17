51c745f02874bd4751a485cc238750fc07a38088
MrigankKumar <bhardwajmrigank13@gmail.com>
2025-08-17 22:29:50 +0530

    Modifies code of second chapter practice set sixth practice set file

--- ch-2-variables-practice-set/sixth-practice-set.py
+++ ch-2-variables-practice-set/sixth-practice-set.py
@@ -1,6 +1,9 @@
 # Write a Python program to calculate the square of a number entered by user
 
 user_input = int(input("Enter the number: "))
-
-square_root = user_input * user_input # To find square you have to multiply that number by itself
+# To find the square root of any number there are two methods 
+#  First Method - number ** 2 
+# Second Method = number * number
+# NOTE - a ^ 2 - is not the method to find the sqaure root in the Python
+square_root = user_input ** 2  # To find square you have to multiply that number by itself
 print(square_root)
